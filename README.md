8dc8af8cfbd89de11a34540d0696d84ad9c9b95c
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-04-17 09:19:54 +0200

    Init commit

--- README.md
+++ README.md
@@ -2,5 +2,34 @@
 
 I have come to love the tool tha AI assisted coding delivers. It doesn´t remove the need for knowing how to code. But the help from the AI expands the help functions we are come to expext from Intellicense in the diffenrent IDEs.
 
+## Libraries and dependencies
+
+### OpenAI API
+
+Først install OpenAI API - pref. in a virtuel environment
+
+    pip install openai
+
+You canb get API ket free for personal use at: [OpenAI - API keys](https://platform.openai.com/account/api-keys)
+
+### pyttsx3
+
+From pypi.org:
+pyttsx3 is a text-to-speech conversion library in Python. Unlike alternative libraries, it works offline, and is compatible with both Python 2 and 3.
+
+    pip install pyttsx3
+
+An example of using pyttsx3
+
+```python
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
+```
+You can find more on the [pyttsx3 GitHub page](https://github.com/nateshmbhat/pyttsx3)
+
 ## References
 * [YouTube - Create a ChatGPT Voice Assistant in 8 Minutes (Python Tutorial)](https://www.youtube.com/watch?v=8z8Cobsvc9k)
+* [OpenAI API reference](https://platform.openai.com/docs/api-reference/introduction)
+* [Pypi.org - pyttsx3 2.90](https://pypi.org/project/pyttsx3/)
--- program.py
+++ program.py
@@ -1,6 +1,76 @@
 import openai
 import pyttsx3
-import speech_recognition ar sr
+import speech_recognition as sr
 import time
 
 # Set you OpenAI API key
+openai.api_key="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"
+
+# Initialize the text-to-speech engine
+engine = pyttsx3.init()
+
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+    try:  
+        return recognizer.recognize_google(audio)
+    except:
+        print("There was an error transcribing the audio to text")
+    
+def generate_response(prompt):
+    response = openai.Completion.create(
+      #engine="davinci",
+      engine="text-davinci-003",
+      prompt=prompt,
+      temperature=0.9,
+      max_tokens=150,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0.6,
+      #stop=["\n", " Human:", " AI:"]
+      stop=None
+    )
+    return response["choices"][0]["text"]
+
+def speak_text(text):
+    engine.say(text)
+    engine.runAndWait()
+    
+def main():
+    while True:
+        # Record audio
+        print("Say Genius to start recording ...")
+        
+        with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
+            try:
+                transcript = recognizer.recognize_google(audio)
+                if transcript.lower == "genius":
+                    # Record audio
+                    filename = "input.wav"
+                    print("Recording ...")
+                    
+                    with open("recording.wav", "wb") as source:
+                        recognizer.adjust_for_ambient_noise(source)
+                        source.pause_threshold = 1
+                        audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
+                        
+                        with open(filename, "wb") as f:
+                            f.write(audio.get_wav_data())
+                            print("Done recording")
+                            
+                    # Transcribe audio to text
+                    text = transcribe_audio_to_text(filename)
+                    if text:
+                        print("Transcription: " + text)
+                        
+                    # Generate response using GPT-3
+                    
+                    
+            except:
+                print("There was an error recording the audio")
+                        
+            
